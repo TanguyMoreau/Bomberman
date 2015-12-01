80e43ca41092cd7ed104ef2ee4891e0aede9b5e9
grochette <grochette@gmlin143>
2015-12-01 13:28:49 +0100

    "change"

--- src/bomberman/elements/geometry/Geometry.java
+++ src/bomberman/elements/geometry/Geometry.java
@@ -18,7 +18,7 @@ public class Geometry{
         private Coordinates position;
         private double radius;
         private Coordinates oldPosition;
-        private Coordinates speed;
+        private Coordinates direction;
 
         public Coordinates getPosition(){
                 return position;
@@ -44,33 +44,33 @@ public class Geometry{
                 this.oldPosition = oldPosition;
         }
 
-        public Coordinates getSpeed(){
-                return speed;
+        public Coordinates getDirection(){
+                return direction;
         }
 
-        public void setSpeed(Coordinates speed){
-                this.speed = speed;
+        public void setDirection(Coordinates direction){
+                this.direction = direction;
         }
 
         public Geometry(){
                 this.position = new Coordinates();
                 this.radius = 0;
                 this.oldPosition = new Coordinates();
-                updateSpeed(1);
+                updateDirection();
         }
         
         public Geometry(double x, double y, double radius){
                 this.position=new Coordinates(x, y);
                 this.oldPosition=this.position;
                 this.radius=radius;
-                updateSpeed(1);
+                updateDirection();
         }
 
         public Geometry(Coordinates position, double radius){
                 this.position = position;
                 this.radius = radius;
                 this.oldPosition = position;
-                updateSpeed(1);
+                updateDirection();
         }
         
         public Geometry(ArrayList<Entity> blockingBodies){
@@ -83,7 +83,7 @@ public class Geometry{
                 }
                 this.radius=r;
                 this.oldPosition=this.position;
-                updateSpeed(1);
+                updateDirection();
         }
 
         public static double distanceInfinity(Coordinates a, Coordinates b){
@@ -103,55 +103,15 @@ public class Geometry{
                 this.position = newPosition;
         }
 
-        public void updateSpeed(double dt){
-                double sX = 0;
-                double sY = 0;
-                try{
-                        sX = (position.getX() - oldPosition.getX()) / dt;
-                        sY = (position.getX() - oldPosition.getY()) / dt;
-                }
-                catch(ArithmeticException ex){
-
-                }
-                this.speed = new Coordinates(sX, sY);
-        }
-
-        @Override
-        public int hashCode(){
-                int hash = 5;
-                hash = 59 * hash + Objects.hashCode(this.position);
-                hash = 59 * hash + (int) (Double.doubleToLongBits(this.radius) ^ (Double.doubleToLongBits(this.radius) >>> 32));
-                hash = 59 * hash + Objects.hashCode(this.oldPosition);
-                hash = 59 * hash + Objects.hashCode(this.speed);
-                return hash;
-        }
-
-        @Override
-        public boolean equals(Object obj){
-                if(obj == null){
-                        return false;
-                }
-                if(getClass() != obj.getClass()){
-                        return false;
-                }
-                final Geometry other = (Geometry) obj;
-                if(!Objects.equals(this.position, other.position)){
-                        return false;
-                }
-                if(Double.doubleToLongBits(this.radius) != Double.doubleToLongBits(other.radius)){
-                        return false;
-                }
-                if(!Objects.equals(this.oldPosition, other.oldPosition)){
-                        return false;
-                }
-                if(!Objects.equals(this.speed, other.speed)){
-                        return false;
-                }
-                return true;
+        public void updateDirection(){
+                double dX = (position.getX() - oldPosition.getX());
+                double dY = (position.getX() - oldPosition.getY());
+                this.direction = new Coordinates(dX, dY);
         }
 
         @Override
         public String toString(){
-                return "Geometry{" + "position=" + position + ", radius=" + radius + ", oldPosition=" + oldPosition + ", speed=" + speed + '}';
+                return "Geometry{" + "position=" + position + ", radius=" + radius + ", oldPosition=" + oldPosition + ", direction=" + direction + '}';
         }
+        
 }
