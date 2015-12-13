4f9539cbbf7f9ac8f3da8f63245c80354d3fe72a
Tanguy <Tanguy@Tanguy-PC>
2015-12-13 15:57:19 +0100

    Ajout des commentaires + Modification createNew

--- src/bomberman/reseau/Client.java
+++ src/bomberman/reseau/Client.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bomberman.reseau;
 
 import bomberman.ClientMain;
@@ -18,41 +13,33 @@ import java.util.ArrayList;
  * @author tmoreau
  */
 public class Client {
-
+    
     InterfaceReseau myInterface;
     int position;
     private ArrayList<EntityLite> myDataBoard;
     private ClientMain thread;
 
-    public Client(ClientMain thread) {
-        this.thread = thread;
-    }
-
+    // Accesseurs
     public ArrayList<EntityLite> getMyDataBoard() {
         return myDataBoard;
     }
-
     public void setMyDataBoard(ArrayList<EntityLite> myDataBoard) {
         this.myDataBoard = myDataBoard;
     }
-
     public InterfaceReseau getMyInterface() {
         return myInterface;
     }
-
     public void setMyInterface(InterfaceReseau myInterface) {
         this.myInterface = myInterface;
     }
-
     public int getPosition() {
         return position;
     }
-
     public void setPosition(int position) {
         this.position = position;
     }
 
-    public InterfaceReseau connectionToServer(String[] args) throws NotBoundException, RemoteException {
+    public InterfaceReseau connectionToServer(String[] args) throws NotBoundException, RemoteException { 
         if (args.length > 0) {
             myInterface = (InterfaceReseau) LocateRegistry.getRegistry(args[0]).lookup("distantServer_02");
         } else {
@@ -61,20 +48,22 @@ public class Client {
         return myInterface;
     }
 
-    public void initialization(InterfaceReseau myInterface) throws RemoteException {
+    public void initialization() throws RemoteException { // Permet la génération dynamique des clients
         myInterface.addNew();
         myInterface.ListOfActionsAdd();
         this.position = myInterface.getActualPosition();
         myInterface.setActualPosition(this.position + 1);
-        myInterface.setCreateNewPos(position, false);
     }
 
-    public void sendDataToServer(InterfaceReseau myInterface, Action myAction, int position) throws RemoteException {
-        //System.out.println(myInterface.getDataBoard());
-        this.setMyDataBoard(myInterface.getDataBoard());
-        myInterface.setCoordinatesPos(position, myAction.getCoordinates());
+    public void sendDataToServer(Action myAction, int position) throws RemoteException {
+        this.setMyDataBoard(myInterface.getDataBoard()); //Acquiert le nouveau board du serveur
+        myInterface.setCoordinatesPos(position, myAction.getCoordinates()); // Envoie la nouvelle action du client
         myInterface.setDropTheBombPos(position, myAction.isDropTheBomb());
-        myInterface.setDataSendPos(position, true);
-        //System.out.println(myInterface.getListOfActions());
+        myInterface.setDataSendPos(position, true); // Permet au serveur de savoir quand récupérer les nouvelles données
+    }
+    
+    // Constructeur
+    public Client(ClientMain thread) {
+        this.thread = thread;
     }
 }
